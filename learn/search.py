a56b1aeeb887b68511d03fa9c6eed3d70d9cef04
YangKui <c6swift@gmail.com>
2018-04-14 21:45:46 +0800

    feat: big index func

--- learn/search.py
+++ learn/search.py
@@ -1,3 +1,5 @@
+from functools import reduce
+import time
 index = []
 
 
@@ -20,3 +22,30 @@ def lookup(index, keyword):
         if record[0] == keyword:
             return record[1]
     return []
+
+
+def make_str(list):
+    return reduce(lambda str, ch: str + ch, list)
+
+
+def make_big_index(size):
+    print(time.clock())
+    index = []
+    letters = ['a', 'a', 'a', 'a', 'a', 'a', 'a']
+    while len(index) < size:
+        word = make_str(letters)
+        add_to_index(index, word, 'fake')
+        for i in range(len(letters) - 1, 0, -1):
+            if letters[i] < 'z':
+                letters[i] = chr(ord(letters[i]) + 1)
+                break
+            else:
+                letters[i] = 'a'
+    print(time.clock())
+    return index
+
+
+myindex = make_big_index(10000)
+start = time.clock()
+lookup(myindex, 'aaaaaaaa')
+print(time.clock() - start)
