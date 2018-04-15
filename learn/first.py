07dcf03626c661154d16a0a413eea918e1a9e0b0
YangKui <c6swift@gmail.com>
2018-04-15 18:09:46 +0800

    feat: new alignments

--- learn/first.py
+++ learn/first.py
@@ -1,19 +1,18 @@
 speed = 299792458
-nano = 10 ** -9
+nano = 10**-9
 
 print(speed * nano * 100)
 
 str = 'wocao'
-print(str[0:-1+3])
+print(str[0:-1 + 3])
 
 print(str.find('a', 2))
 
 print(round(6.43343))
 
-
 print(str[::-1] == str)
 
-print(2 ** 100)
+print(2**100)
 
 
 def print_multiplication_table(n):
@@ -23,3 +22,13 @@ def print_multiplication_table(n):
 
 
 print_multiplication_table(4)
+
+
+def fibonacci(n, a=0, b=1):
+    if n <= 1:
+        return b
+    return fibonacci(n - 1, b, a + b)
+
+
+for i in range(1, 40):
+    print(fibonacci(i))
--- learn/hash.py
+++ learn/hash.py
@@ -2,7 +2,8 @@ from functools import reduce
 
 
 def make_hashtable(nbuckets):
-    return reduce(lambda hashtable, _: hashtable + [[]], range(0, nbuckets), [])
+    return reduce(lambda hashtable, _: hashtable + [[]], range(0, nbuckets),
+                  [])
 
 
 def hash_string(keyword, buckets):
@@ -38,3 +39,107 @@ def hashtable_update(htable, key, value):
 hashtable = make_hashtable(3)
 hashtable_add(hashtable, 'fuck', 123)
 print(hashtable)
+
+courses = {
+    'june2043': {
+        'cs111': {
+            'name': 'Historical Computers - Before Voice Activation',
+            'teacher': 'Scotty'
+        },
+        'cs312': {
+            'assistant': 'Amy',
+            'name': 'Build your own Time Machine',
+            'teacher': 'Melody'
+        }
+    },
+    'apr2012': {
+        'cs262': {
+            'assistant': 'Peter C.',
+            'prereq': 'cs101',
+            'name': 'Programming Languages - Building a Web Browser',
+            'teacher': 'Wes'
+        },
+        'cs101': {
+            'assistant': 'Sarah',
+            'name': 'Building a Search Engine',
+            'teacher': 'Dave'
+        },
+        'cs253': {
+            'prereq': 'cs101',
+            'name': 'Web Application Engineering - Building a Blog',
+            'teacher': 'Steve'
+        },
+        'cs373': {
+            'name': 'Programming a Robotic Car',
+            'teacher': 'Sebastian'
+        },
+        'cs212': {
+            'assistant': 'Andy',
+            'prereq': 'cs101',
+            'name': 'The Design of Computer Programs',
+            'teacher': 'Peter N.'
+        },
+        'cs387': {
+            'name': 'Applied Cryptography',
+            'teacher': 'Dave'
+        }
+    },
+    'jan2044': {
+        'cs003': {
+            'assistant': 'Amy',
+            'name': 'Programming a Robotic Robotics Teacher',
+            'teacher': 'Jasper'
+        },
+        'cs001': {
+            'name': 'Building a Quantum Holodeck',
+            'teacher': 'Dorina'
+        },
+        'cs312': {
+            'assistant': 'Amy',
+            'name': 'Build your own Time Machine',
+            'teacher': 'Melody'
+        }
+    },
+    'feb2012': {
+        'cs101': {
+            'assistant': 'Peter C.',
+            'name': 'Building a Search Engine',
+            'teacher': 'Dave'
+        },
+        'cs373': {
+            'assistant': 'Andy',
+            'name': 'Programming a Robotic Car',
+            'teacher': 'Sebastian'
+        }
+    }
+}
+
+
+def involved(courses, person):
+    result = {}
+    for (time, detail) in courses.items():
+        for (course, information) in detail.items():
+            if information.get('assistant', information['teacher']) == person:
+                key = result.get(time, [])
+                key.append(course)
+                result[time] = key
+
+    return result
+
+
+print(involved(courses, 'Scotty'))
+
+
+def cached_execution(cache, proc, proc_input):
+    # Your code here
+    hadCached = cache.get(proc_input)
+    if hadCached:
+        return hadCached
+
+    result = proc(proc_input)
+    cache[proc_input] = result
+
+    return result
+
+
+print(cached_execution({}, lambda a: a * 2, 2))
